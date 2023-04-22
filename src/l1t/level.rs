2eae40bc609d2569398a7c0a6a48731d5646d752
Alexander Lay-Calvert <alex.laycalvert@gmail.com>
2023-04-22 10:47:47 -0400

    Implemented buttons, togle blocks,zappers, and statues

--- src/l1t/level.rs
+++ src/l1t/level.rs
@@ -28,7 +28,7 @@ impl Level {
         for i in 0..self.nodes.len() {
             self.nodes[i].looking_at.clear();
             let node = &self.nodes[i];
-            if !matches!(node.node_type, NodeType::Laser(_)) {
+            if !matches!(node.node_type, NodeType::Laser(_, _)) || self.nodes[i].toggled {
                 continue;
             }
             let mut current_row: i16 = node.row as i16;
@@ -61,26 +61,51 @@ impl Level {
                     arrow_ch,
                 ));
                 if let Some(j) = self.get_node_by_position(current_row as u16, current_col as u16) {
-                    let node_at_pos = &self.nodes[j];
-                    if !matches!(node_at_pos.node_type, NodeType::Mirror(_)) {
-                        break;
-                    }
-                    // Determines what the new direction is after encountering a mirror
-                    if current_dir.0 == 0 {
-                        current_dir.0 = current_dir.1.abs();
-                        if current_dir.1 == (node_at_pos.dir.0 + node_at_pos.dir.1) {
-                            current_dir.0 = -current_dir.0
+                    match self.nodes[j].node_type {
+                        NodeType::Statue | NodeType::Zapper => {
+                            self.nodes[j].toggled = true;
+                            break;
                         }
-                        current_dir.1 = 0;
-                    } else {
-                        current_dir.1 = current_dir.0.abs();
-                        if current_dir.0 == (node_at_pos.dir.0 + node_at_pos.dir.1) {
-                            current_dir.1 = -current_dir.1
+                        NodeType::Laser(_, _) => {
+                            self.nodes[j].toggled = true;
+                            self.nodes[j].looking_at.clear();
+                            break;
                         }
-                        current_dir.0 = 0;
+                        NodeType::Mirror(_) => {
+                            // Determines what the new direction is after encountering a mirror
+                            let node_at_pos = &self.nodes[j];
+                            if current_dir.0 == 0 {
+                                current_dir.0 = current_dir.1.abs();
+                                if current_dir.1 == (node_at_pos.dir.0 + node_at_pos.dir.1) {
+                                    current_dir.0 = -current_dir.0
+                                }
+                                current_dir.1 = 0;
+                            } else {
+                                current_dir.1 = current_dir.0.abs();
+                                if current_dir.0 == (node_at_pos.dir.0 + node_at_pos.dir.1) {
+                                    current_dir.1 = -current_dir.1
+                                }
+                                current_dir.0 = 0;
+                            }
+                        }
+                        NodeType::ToggleBlock => {
+                            if !self.nodes[j].toggled {
+                                break;
+                            }
+                        }
+                        _ => break,
                     }
                 }
             }
+        }
+    }
+
+    fn reset_statues(&mut self) {
+        for i in 0..self.nodes.len() {
+            if !matches!(self.nodes[i].node_type, NodeType::Statue) {
+                continue;
+            }
+            self.nodes[i].toggled = false
         }
     }
 
@@ -185,14 +210,14 @@ impl Level {
 
     fn draw_laser_overlays(&self, stdout: &mut Stdout) {
         for (_, n) in self.nodes.iter().enumerate() {
-            if !matches!(n.node_type, NodeType::Laser(_)) {
+            if !matches!(n.node_type, NodeType::Laser(_, _)) {
                 continue;
             }
             for i in 0..n.looking_at.len() {
                 let pos = &n.looking_at[i];
                 execute!(
                     stdout,
-                    SetForegroundColor(Color::Red),
+                    SetForegroundColor(Color::Rgb { r: 255, g: 0, b: 0 }),
                     cursor::MoveTo(pos.1 + self.col_offset, pos.0 + self.row_offset),
                     Print(if i == n.looking_at.len() - 1 {
                         pos.3
@@ -223,7 +248,7 @@ impl Level {
         let players_next_col = (self.nodes[player_index].col as i16 + dir.1) as u16;
         if let Some(i) = self.get_node_by_position(players_next_row, players_next_col) {
             match self.nodes[i].node_type {
-                NodeType::Block | NodeType::Mirror(_) | NodeType::Laser(_) => {
+                NodeType::Block | NodeType::Mirror(_) | NodeType::Laser(_, _) => {
                     let blocks_next_row = (players_next_row as i16 + dir.0) as u16;
                     let blocks_next_col = (players_next_col as i16 + dir.1) as u16;
                     if let Some(_) = self.get_node_by_position(blocks_next_row, blocks_next_col) {
@@ -240,10 +265,6 @@ impl Level {
                     self.nodes[i].row = blocks_next_row;
                     self.nodes[i].col = blocks_next_col;
                 }
-                NodeType::Switch => {
-                    self.nodes[i].toggled = !self.nodes[i].toggled;
-                    //self.toggle_blocks();
-                }
                 NodeType::ToggleBlock => {
                     if self.nodes[i].toggled {
                         self.set_player_position(players_next_row, players_next_col);
@@ -254,6 +275,103 @@ impl Level {
         } else {
             self.set_player_position(players_next_row, players_next_col);
         }
+        for i in 0..self.nodes.len() {
+            if matches!(self.nodes[i].node_type, NodeType::Button) {
+                if self.nodes[i].toggled {
+                    self.toggle_blocks();
+                }
+                self.nodes[i].toggled = false;
+                if (self.nodes[i].row == self.nodes[player_index].row
+                    && (self.nodes[i].col == self.nodes[player_index].col - 1
+                        || self.nodes[i].col == self.nodes[player_index].col + 1))
+                    || (self.nodes[i].col == self.nodes[player_index].col
+                        && (self.nodes[i].row == self.nodes[player_index].row - 1
+                            || self.nodes[i].row == self.nodes[player_index].row + 1))
+                {
+                    if !self.nodes[i].toggled {
+                        self.toggle_blocks();
+                    }
+                    self.nodes[i].toggled = true;
+                }
+            }
+        }
+    }
+
+    fn get_nodes_surrounding_player(&self) -> Vec<usize> {
+        let mut nodes = vec![];
+        let player_index = match self.get_player() {
+            Some(i) => i,
+            None => return nodes,
+        };
+        for i in 0..self.nodes.len() {
+            if (self.nodes[i].row == self.nodes[player_index].row
+                && (self.nodes[i].col == self.nodes[player_index].col - 1
+                    || self.nodes[i].col == self.nodes[player_index].col + 1))
+                || (self.nodes[i].col == self.nodes[player_index].col
+                    && (self.nodes[i].row == self.nodes[player_index].row - 1
+                        || self.nodes[i].row == self.nodes[player_index].row + 1))
+            {
+                nodes.push(i);
+            }
+        }
+        nodes
+    }
+
+    fn player_action(&mut self) {
+        for (_, &i) in self.get_nodes_surrounding_player().iter().enumerate() {
+            match self.nodes[i].node_type {
+                NodeType::Button | NodeType::Statue => continue,
+                NodeType::Mirror(_) => {
+                    if matches!(self.nodes[i].dir, Direction::LEFT) {
+                        self.nodes[i].dir = Direction::RIGHT;
+                        self.nodes[i].ch = '/';
+                        continue;
+                    }
+                    self.nodes[i].dir = Direction::LEFT;
+                    self.nodes[i].ch = '\\';
+                }
+                _ => {
+                    self.nodes[i].toggled = !self.nodes[i].toggled;
+                    match self.nodes[i].node_type {
+                        NodeType::Switch => self.toggle_blocks(),
+                        _ => (),
+                    }
+                }
+            }
+        }
+    }
+
+    fn toggle_blocks(&mut self) {
+        for i in 0..self.nodes.len() {
+            if !matches!(self.nodes[i].node_type, NodeType::ToggleBlock) {
+                continue;
+            }
+            self.nodes[i].toggled = !self.nodes[i].toggled;
+        }
+    }
+
+    fn all_statues_lit(&self) -> bool {
+        for (_, n) in self.nodes.iter().enumerate() {
+            if !matches!(n.node_type, NodeType::Statue) {
+                continue;
+            }
+            if !n.toggled {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn any_zappers_lit(&self) -> bool {
+        for (_, n) in self.nodes.iter().enumerate() {
+            if !matches!(n.node_type, NodeType::Zapper) {
+                continue;
+            }
+            if n.toggled {
+                return true;
+            }
+        }
+        false
     }
 
     pub fn new(filename: String, term_rows: u16, term_cols: u16) -> Result<Level, &'static str> {
@@ -304,8 +422,20 @@ impl Level {
     pub fn play(&mut self, stdout: &mut Stdout) -> Result<bool, &str> {
         enable_raw_mode().ok();
         stdout.execute(cursor::Hide).ok();
+        let mut has_won = false;
         loop {
+            self.reset_statues();
             self.set_lasers_looking_at();
+
+            if self.all_statues_lit() {
+                has_won = true;
+                break;
+            }
+
+            if self.any_zappers_lit() {
+                break;
+            }
+
             self.draw(stdout);
             match read().unwrap() {
                 Event::Key(event) => {
@@ -314,12 +444,12 @@ impl Level {
                         KeyCode::Char('s') => self.move_player(Direction::DOWN),
                         KeyCode::Char('a') => self.move_player(Direction::LEFT),
                         KeyCode::Char('d') => self.move_player(Direction::RIGHT),
+                        KeyCode::Char(' ') => self.player_action(),
                         //KeyCode::Up => self.change_player_direction(Direction::UP),
                         //KeyCode::Down => self.change_player_direction(Direction::DOWN),
                         //KeyCode::Left => self.change_player_direction(Direction::LEFT),
                         //KeyCode::Right => self.change_player_direction(Direction::RIGHT),
                         //KeyCode::Tab => self.toggle_portal(),
-                        //KeyCode::Char(' ') => self.shoot_portal(),
                         KeyCode::Char('q') => break,
                         _ => (),
                     }
@@ -329,6 +459,6 @@ impl Level {
         }
         stdout.execute(cursor::Show).ok();
         disable_raw_mode().ok();
-        Ok(false)
+        Ok(has_won)
     }
 }
--- src/l1t/node.rs
+++ src/l1t/node.rs
@@ -10,7 +10,9 @@ pub enum NodeType {
     ToggleBlock,
     Button,
     Mirror(Direction),
-    Laser(Direction),
+    Laser(Direction, bool),
+    Statue,
+    Zapper,
 }
 
 impl NodeType {
@@ -19,15 +21,21 @@ impl NodeType {
             'X' => Some(NodeType::Player),
             'K' => Some(NodeType::Block),
             'I' => Some(NodeType::Wall),
-            'S' => Some(NodeType::Switch),
-            'B' => Some(NodeType::Button),
+            's' => Some(NodeType::Switch),
+            'b' => Some(NodeType::Button),
             'T' => Some(NodeType::ToggleBlock),
             '/' => Some(NodeType::Mirror(Direction::RIGHT)),
             '\\' => Some(NodeType::Mirror(Direction::LEFT)),
-            '1' => Some(NodeType::Laser(Direction::UP)),
-            '2' => Some(NodeType::Laser(Direction::DOWN)),
-            '3' => Some(NodeType::Laser(Direction::LEFT)),
-            '4' => Some(NodeType::Laser(Direction::RIGHT)),
+            '1' => Some(NodeType::Laser(Direction::UP, true)),
+            '2' => Some(NodeType::Laser(Direction::DOWN, true)),
+            '3' => Some(NodeType::Laser(Direction::LEFT, true)),
+            '4' => Some(NodeType::Laser(Direction::RIGHT, true)),
+            '5' => Some(NodeType::Laser(Direction::UP, false)),
+            '6' => Some(NodeType::Laser(Direction::DOWN, false)),
+            '7' => Some(NodeType::Laser(Direction::LEFT, false)),
+            '8' => Some(NodeType::Laser(Direction::RIGHT, false)),
+            'S' => Some(NodeType::Statue),
+            'Z' => Some(NodeType::Zapper),
             _ => None,
         }
     }
@@ -58,12 +66,43 @@ impl Node {
 
     pub fn new(node_type: NodeType, row: u16, col: u16) -> Node {
         let mut dir = Direction::UP;
+        let mut toggled = false;
         let (ch, fg_color, bg_color, toggled_fg_color, toggled_bg_color) = match node_type {
             NodeType::Player => ('X', Color::Green, Color::Green, Color::Green, Color::Green),
             NodeType::Block => ('K', Color::Grey, Color::Grey, Color::Grey, Color::Grey),
             NodeType::Wall => ('I', Color::White, Color::White, Color::White, Color::White),
-            NodeType::Switch => ('S', Color::Yellow, Color::Red, Color::Red, Color::Yellow),
-            NodeType::Button => ('B', Color::Yellow, Color::Red, Color::Red, Color::Yellow),
+            NodeType::Switch => ('s', Color::Black, Color::Red, Color::Black, Color::Yellow),
+            NodeType::Button => ('b', Color::Black, Color::Red, Color::Black, Color::Yellow),
+            NodeType::Zapper => (
+                'Z',
+                Color::Yellow,
+                Color::Black,
+                Color::Yellow,
+                Color::Black,
+            ),
+            NodeType::Statue => (
+                'S',
+                Color::Rgb {
+                    r: 100,
+                    g: 100,
+                    b: 0,
+                },
+                Color::Rgb {
+                    r: 100,
+                    g: 100,
+                    b: 0,
+                },
+                Color::Rgb {
+                    r: 255,
+                    g: 255,
+                    b: 0,
+                },
+                Color::Rgb {
+                    r: 255,
+                    g: 255,
+                    b: 0,
+                },
+            ),
             NodeType::ToggleBlock => (
                 'T',
                 Color::Magenta,
@@ -85,9 +124,16 @@ impl Node {
                     Color::Reset,
                 )
             }
-            NodeType::Laser(d) => {
+            NodeType::Laser(d, on) => {
                 dir = d;
-                ('L', Color::Red, Color::Red, Color::Red, Color::Red)
+                toggled = !on;
+                (
+                    'L',
+                    Color::Rgb { r: 255, g: 0, b: 0 },
+                    Color::Rgb { r: 255, g: 0, b: 0 },
+                    Color::Rgb { r: 150, g: 0, b: 0 },
+                    Color::Rgb { r: 150, g: 0, b: 0 },
+                )
             }
         };
 
@@ -101,7 +147,7 @@ impl Node {
             toggled_fg_color,
             toggled_bg_color,
             dir,
-            toggled: false,
+            toggled,
             looking_at: vec![],
         }
     }
