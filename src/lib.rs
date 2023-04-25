09114bd1af451ac19134f84beb26480dfe41f3d1
Alexander Lay-Calvert <alex.laycalvert@gmail.com>
2023-04-24 23:18:11 -0400

    Updated package

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! # l1t
+//!
+//! A strategy game about shooting laser beams and lighting up statues.
+//! Checkout out [the repo](https://github.com/alex-laycalvert/l1t) for
+//! more info.
 pub mod direction;
 pub mod level;
 pub mod menu;
