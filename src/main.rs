b0e6328868d6fe54da712570415a47feec6770ac
Alexander Lay-Calvert <alex.laycalvert@gmail.com>
2023-04-25 16:45:41 -0400

    Added clap for args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use crossterm::{
     cursor,
     terminal::{disable_raw_mode, enable_raw_mode, Clear},
@@ -7,21 +8,32 @@ use home::home_dir;
 use l1t::level::*;
 use l1t::menu::*;
 use l1t::userdata::*;
-use std::env::args;
 use std::io::stdout;
 use std::{thread, time};
 
 const SLEEP_TIME: u64 = 500;
 
+/// A terminal based strategy game about shooting lasers and lighting statues
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The `.l1t` file to load a level from
+    #[arg(short, long)]
+    file: Option<String>,
+    ///// Repository to download levels from
+    //#[arg(short, long)]
+    //repo_url: Option<String>,
+}
+
 fn main() {
+    let args = Args::parse();
     let mut stdout = stdout();
     enable_raw_mode().ok();
     stdout.execute(cursor::Hide).ok();
-    let args: Vec<String> = args().collect();
-    if args.len() > 1 {
+    if let Some(filename) = &args.file {
         // File has been provided
         loop {
-            let mut level = match Level::file(args[1].to_string()) {
+            let mut level = match Level::file(filename.to_string()) {
                 Ok(l) => l,
                 Err(e) => {
                     eprintln!("{e}");
